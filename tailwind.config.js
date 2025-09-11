2030da069066f9e96ea736b073c7910874aaa645
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-11 23:09:11 +0100

    Navbar issues, minor changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#171717', // or different shades if you want
+        },
+      },
+    },
+  },
+};
